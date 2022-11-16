6e01f136ae3b8c0f3d23938291b28be9a98093a8
mohamed <himantik@yahoo.com>
2022-11-16 23:27:13 +0000

    playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,28 @@
+print(__name__)
+print("Module #1 Name=", __name__)
+def main():
+    print("Module #1 Name=", __name__)
+
+if __name__ == "__main__":
+    main()
+if __name__ == "__main__":
+   print("Code is being run directly from Python.")
+
+else:
+   print("Code is being run indirectly from import.")
+
+print("This code will always execute.")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+   main()
+print("This code will always execute.")
+
+def main():
+   print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+   main()
+
---
+++ if_name/second_module.py
@@ -0,0 +1,16 @@
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+print("Module #2 Name=", __name__)
+# the name of the import matches the file name (minus the .py)
+# these two files MUST BE IN THE SAME DIRECTORY
+import first_module
+
+first_module.main() # ADD THIS LINE
+
+print("Module #2 Name=", __name__)
+
