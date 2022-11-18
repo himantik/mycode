1719bf4e832bef87307aae0801203f8ff58a0f14
mohamed <himantik@yahoo.com>
2022-11-18 23:20:54 +0000

    Learning to parse log files

---
+++ miniproject.py
@@ -0,0 +1,16 @@
+
+
+
+
+print("Welcome to my  mini project!!!")
+print("Today we will be presenting grammy to the Cohorts")
+print("Keep your hands together as I present the host of the day: Mohamed")
+
+print("First nomination of the day: The funniest sayings:" , " A: If you talk I can't hear you"  , "B: Whose name in the screen is Hi" ,"C: The fan is loud" ," D: Can you hear me now", )
+
+print("a = \n", a)
+
+answer= input("The Award goes to")
+if answer == "The fan is loud":
+    print("Congratulations")
+
