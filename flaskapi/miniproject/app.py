13d8dcc52f0276ec33a487a3edbc18df10487775
himantik <himantik@yahoo.com>
2022-11-29 18:22:10 +0000

    Late changes

--- flaskapi/miniproject/app.py
+++ flaskapi/miniproject/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,
+jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
 tasks = ["Make miniproject for Flask", "Finish all assignments", "Build a task app", "Have a good night sleep"]
+
+#@app.route('/form-handler', methods=['POST'])
+#def handle_data():
+#   return jsonify(request.form)
 
 class TaskForm(FlaskForm):
     task = StringField("Task")
@@ -18,8 +23,6 @@ def index():
 
     return render_template('index.html', tasks=tasks, template_form=TaskForm()) 
 
-#URL= "http://127.0.0.1:2224/map#
-#pprint(requests.get(URL).json())#
 
 
 if __name__ == "__main__":
