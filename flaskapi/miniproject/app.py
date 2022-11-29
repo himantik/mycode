1ea055848f07143f4bc3d04da830c11ae718f0b9
himantik <himantik@yahoo.com>
2022-11-29 20:19:50 +0000

    push

--- flaskapi/miniproject/app.py
+++ flaskapi/miniproject/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request,
-jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
